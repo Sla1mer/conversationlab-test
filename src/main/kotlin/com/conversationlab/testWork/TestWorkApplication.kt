b40982d931c399aa84ff627fbb5fc728005b35ef
Nazar <nazar@mypointonline.ru>
2024-06-19 16:07:33 +0200

    init project

---
+++ src/main/kotlin/com/conversationlab/testWork/TestWorkApplication.kt
@@ -0,0 +1,11 @@
+package com.conversationlab.testWork
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TestWorkApplication
+
+fun main(args: Array<String>) {
+	runApplication<TestWorkApplication>(*args)
+}
